6a4d5b54b939a6c9dbcc582ca9cfe7c43711c593
agapodo <dochitime22@gmail.com>
2026-06-07 23:38:12 +0900

    startGame.bat 주소 변경

--- StartGame.bat
+++ StartGame.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting local server for Boo!m Night...
-start http://localhost:8000/Boom%%20Night_demo1.html
+start http://localhost:8000/index.html
 python -m http.server 8000
 pause
